f16330768938ca3a16472446cdbf0e8b6ae467dc
Francisco Sepulveda <fsepulveda@copade.neuquen.gov.ar>
2023-03-16 14:56:51 -0300

    corssss

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,3 +1,8 @@
-const allowedOrigins = ["*"];
+const allowedOrigins = [
+  "https://biblioarcoiris-admin.vercel.app",
+  "http://127.0.0.1:5500",
+  "http://localhost:3000",
+  "http://localhost:5173",
+];
 
 module.exports = allowedOrigins;
